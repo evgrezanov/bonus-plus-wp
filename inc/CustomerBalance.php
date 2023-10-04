d8760a9b0f955595f733291457274adfe7302e84
Mikhail Kuznetsov <mickuznetsoff@gmail.com>
2023-10-04 18:10:32 +0400

    retail

--- inc/CustomerBalance.php
+++ inc/CustomerBalance.php
@@ -6,7 +6,6 @@ defined('ABSPATH') || exit; // Exit if accessed directly
 
 class BPWPCustomerBalance
 {
-
     /**
      *  Init
      */
@@ -18,85 +17,93 @@ class BPWPCustomerBalance
 
     /**
      *  Начисляем бонусы клиенту
-     *  
-     *  @return object /customer/{phoneNumber}/balance https://bonusplus.pro/api/Help/Api/PATCH-customer-phoneNumber-balance
-     * 
      */
     public static function bpwp_customer_balance_bonusplus($order_id)
     {
         $order = wc_get_order($order_id);
+        $user_id = $order->get_user_id();
 
-        // Получим бонусы заказа для этого клиента.
-        $amount = self::bpwp_get_order_bonuses($order_id);
-        
-        if (is_array($amount) && isset($amount['request']) && is_array($amount['request']['discount'])) {
-            foreach ($amount['request']['discount'] as $discount) {
-                if (isset($discount['cb']) && !empty($discount['cb'])){
-                    $sum_bonuses = $discount['cb']; 
-                }
-            }
+        //Проведение продажи в БонусПлюс
+        $retail = self::bpwp_get_order_bonuses($order_id);
+
+        // Обновление данных пользователя
+        if ($retail['code'] == 200){
+            update_user_meta($user_id, 'bonus-plus', $retail['request']['customer']);
         }
-        
-        $billingPhone  = $order->get_billing_phone();
-
-        // TODO: Сделать проверку, есть ли чел в системе бунус+ (?)
-        // $user_bonusplus = self::bpwp_check_user_bonusplus_account($billingPhone)
-
-        $params = [
-            'amount' => $sum_bonuses,
-        ];
-
-        if (!empty($billingPhone)){
-            $addbounuses = bpwp_api_request(
-                'customer/'. $billingPhone .'/balance',
-                json_encode($params),
-                'PATCH',
-            );
-            $response_code = wp_remote_retrieve_response_code($addbounuses);
-        }
-        
     }
 
+    /**
+     *  Проведение продажи в БонусПлюс
+     *  
+     *  @return object POST /retail https=>//bonusplus.pro/api/Help/Api/POST-retail
+     * 
+     */
     public static function bpwp_get_order_bonuses($order_id)
     {
-        // Переберем товары в заказе (и суммируем бонусы?)
-        $order = wc_get_order($order_id);
-        $products = $order->get_items();
-        $items = [];
 
-		foreach ($products as $product) {
-			$items[] = $product->get_product_id();
-		}
-
-        $items = BPWPApiHelper::bpwp_product_to_retailitems($items);
+        $items = self::bpwp_products_to_retail($order_id);
 
         $store = !empty(get_option('bpwp_shop_name')) ? esc_html(get_option('bpwp_shop_name')) : '';
-
+        
         $billingPhone = bpwp_api_get_customer_phone();
+
         $params = [
             'phone'         => $billingPhone,
             'bonusDebit'    => 0.0,
-            'level'         => 0,
             'store'         => $store,
+            'externalId'    => $order_id, // должно быть уникальным, это идентификатор продажи из внешней системы
             'certificate'   => true
         ];
 
         $params['items'] = $items;
 
-        // Отправим запрос и получим сумму бонусов
+        // Отправим запрос "Проведение продажи в БонусПлюс"
         if (!empty($billingPhone) && !empty($store) && count($items) >= 1){
-            $retailcalc = bpwp_api_request(
-                'retail/calc',
+            $retail = bpwp_api_request(
+                'retail',
                 json_encode($params),
-                'PUT',
+                'POST',
             );
             
-            $response_code = wp_remote_retrieve_response_code($retailcalc);
-            
-            return $retailcalc;
+            return $retail;
         }
 
 	}
+
+    /**
+    * Собираем массив товаров в заказе
+    */
+    public static function bpwp_products_to_retail($order_id) 
+    {
+        $order = wc_get_order($order_id);
+        $items = $order->get_items();
+        $products_data = [];
+        $ext = 0;
+        
+        // Переберем товары в заказе
+        foreach ($items as $item_id => $item) {
+
+            $ext++;
+            $product_id = $item->get_product_id();
+            $product = $item->get_product();
+            
+            $product_price = $product->get_price();
+            $quantity = $item->get_quantity();
+            $total = $item->get_total();
+
+            $product_data = [
+                "sum"       => (float)$total,
+                "qnt"       => (float)$quantity,
+                "product"   => $product_id,
+                "ds"        => 0.0,
+                "ext"       => $order_id.'-'.$ext, //ext - уникальный идентификатор позиции, его нужно делать либо уникальным, либо не заполнять вовсе
+                "price"     => (float) $product_price,
+            ];
+
+            $products_data[] = $product_data;
+        }
+        return $products_data;        
+    }
 }
 
 BPWPCustomerBalance::init();
